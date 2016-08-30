7dd69d7547505bdcb4602bddeef80a13426f77e8
ARVIND <arvindkmr2393@gmail.com>
2016-08-30 13:03:28 +0530

    Update battleship.js

--- battleship.js
+++ battleship.js
@@ -2,19 +2,19 @@ var view= {
 	displaymessage: function(msg){
 		var messageArea= document.getElementById("messageArea");
 		messageArea.innerHTML=msg; 
-	}
+	},
 	displayhit: function(location)
 	{
 		var cell=document.getElementById(location);
-		cell.setAttribute("class", "hit")
-	}
+		cell.setAttribute("class", "hit");
+	},
 	
 	displaymiss: function(location)
 	{
 	var cell =document.getElementById(location);
 	cell.setAttribute("class", "miss");
 	}	
-}
+};
 
 var model={
 	boardsize:7,
@@ -30,15 +30,14 @@ var model={
 		
 		for(var i=0;i<this.numShips;i++)
 		{
-			var ships=this.ships[i];
-			var location=ships.location;
-			var index= location.indexOf(guess);
+			var ship=this.ships[i];
+			var index= ship.location.indexOf(guess);
 			if(index >=0)
 			{
-				this.ships.hits[index]="hit";
-				view.displaymessage("ITS A HIT..!);
+				ship.hits[index]="hit";
+				view.displaymessage("ITS A HIT..!");
 				view.displayhit(guess);
-				if(this.isSunk(ships))
+				if(this.isSunk(ship))
 				{
 					this.shipSunk++;
 					view.displaymessage("You Sank My BattleShip..!");
@@ -50,25 +49,40 @@ var model={
 		view.displaymessage("You Missed..!")
 		return false;
 		
-	}
+	},
 	
-	isSunk:function(ships){
+	isSunk:function(ship){
 		
 		for(var i=0:i<this.shiplength;i++){
-			if(ships.hits[i]!="hit")
+			if(ship.hits[i]!=="hit")
 			{
 				return false;
 			}
 		}
 		return true;
 	}
-}
+};
 
 var controller={
 	
-	guesses: 0;
+	guesses: 0,
 	
-	parseGuess : function(guess){
+	
+	processGuess : function(guess){
+		var valid=parseGuess(guess);
+		if(valid)
+		{
+			this.guesses++;
+			var hit=model.fire(valid);
+			if(hit && model.shipSunk===3)
+			{
+				view.displaymessage("You Sank All My BattleShips in "+this.guesses+ " guesses.");
+			}
+		}
+	}
+};
+
+function parseGuess(guess){
 		
 		var alphabets= ["A","B","C","D","E","F","G"];
 		if(guess===null || guess.length!==2)
@@ -79,7 +93,7 @@ var controller={
 		var first=guess.charAt(0);
 		var row= alphabets.indexOf(first);
 		var column=guess.charAt(1);
-		if(isNaN(row) || column<0 || column>6 || isNaN(column))
+		if(isNaN(row) || column<0 || column>6 || isNaN(column) || row>6 || row<0)
 		{
 			return null;
 		}
@@ -88,26 +102,10 @@ var controller={
 		}
 		
 		}
-	},
-	
-	processGuess : function(guess){
-		var valid=parseGuess(guess);
-		if(valid!==null)
-		{
-			this.guesses++;
-			model.fire(valid);
-			if(model.shipSunk===3)
-			{
-				view.displaymessage("You Sank All My BattleShips in "+this.guesses+ " guesses.");
-			}
-		}
-		else{
-			alert("Enter a valid location.");
-		}
+		return null;
 	}
-}
 
-function init(){
+/* function init(){
 	var inputButton= document.getElementById("fireButton");
 	inputButton.onclick= fireHandler;
 	}
@@ -116,7 +114,18 @@ function fireHandler(){
 	var input=document.getElementById("guessInput");
 	var guess=input.value;
 	controller.processGuess(guess);
-	input.value="";
+	input.value=" ";
 }
 
-window.onload= init;
+window.onload= init; */
+
+controller.processGuess("A0"); // miss
+controller.processGuess("A6"); // hit
+controller.processGuess("B6"); // hit
+controller.processGuess("C6"); // hit
+controller.processGuess("C4"); // hit
+controller.processGuess("D4"); // hit
+controller.processGuess("E4"); // hit
+controller.processGuess("B0"); // hit
+controller.processGuess("B1"); // hit
+controller.processGuess("B2"); // hit
